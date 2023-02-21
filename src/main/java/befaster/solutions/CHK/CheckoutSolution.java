097a913a0397756da9b89a45b444af9b147a580c
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 13:53:10 +0000

    Tue Feb 21 13:53:10 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,12 +2,17 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.AbstractMap;
 import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        Map<String,Integer> prices=new Map.
+        Map<String,Integer> prices=Map.ofEntries(
+                new AbstractMap.SimpleEntry<String,Integer>("A",)
+        );
+
     }
 }
+
 
