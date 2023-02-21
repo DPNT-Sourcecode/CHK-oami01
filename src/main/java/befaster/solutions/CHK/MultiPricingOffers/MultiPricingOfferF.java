0845c1d3352de90010771db534ffb6263e445be2
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 15:34:47 +0000

    Tue Feb 21 15:34:47 UTC 2023

--- src/main/java/befaster/solutions/CHK/MultiPricingOffers/MultiPricingOfferF.java
+++ src/main/java/befaster/solutions/CHK/MultiPricingOffers/MultiPricingOfferF.java
@@ -8,13 +8,14 @@ public class MultiPricingOfferF implements MultiPricingOffer{
         String product="F";
         Integer quantity=basket.get(product);
         if(basket.get(product)>=3) {
-            Integer newValue=basket.get("F")-quantity/2;
+            Integer newValue=basket.get("F")-quantity/3;
             basket.remove("F");
             basket.put("F",newValue);
         }
         return basket;
     }
 }
+
 
 
 
