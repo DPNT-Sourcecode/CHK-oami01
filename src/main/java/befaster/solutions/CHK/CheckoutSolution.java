eeabea8ebc24fa570de36a96957191e5dee0cbf4
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 14:34:27 +0000

    Tue Feb 21 14:34:27 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -20,39 +20,38 @@ public class CheckoutSolution {
         prices.put("D", 15);
         prices.put("", 0);
 
-        Map<Pair<String,Integer>, Integer> specialOffers = new HashMap<>();
-        specialOffers.put(new Pair<String,Integer>("A",3), 130);
-        specialOffers.put(new Pair<String,Integer>("B",2),45);
+        Map<Pair<String, Integer>, Integer> specialOffers = new HashMap<>();
+        specialOffers.put(new Pair<String, Integer>("A", 3), 130);
+        specialOffers.put(new Pair<String, Integer>("B", 2), 45);
 
-        Map<String,Integer> basket=new HashMap<>();
+        Map<String, Integer> basket = new HashMap<>();
 
-        for(int i=0;i<skus.length();i++)
-        {
-            String currentProduct= String.valueOf(skus.charAt(i));
-            if(prices.containsKey(currentProduct)==false)
-            {
+        for (int i = 0; i < skus.length(); i++) {
+            String currentProduct = String.valueOf(skus.charAt(i));
+            if (prices.containsKey(currentProduct) == false) {
                 return -1;
             }
 
-            if(basket.containsKey(currentProduct))
-            {
-                basket.replace(currentProduct,basket.get(currentProduct)+1);
-            }
-            else
-            {
-                basket.put(currentProduct,1);
+            if (basket.containsKey(currentProduct)) {
+                basket.replace(currentProduct, basket.get(currentProduct) + 1);
+            } else {
+                basket.put(currentProduct, 1);
             }
         }
 
-        int total=0;
-        for(Map.Entry<String,Integer> entry:basket.entrySet())
-        {
-            
+        int total = 0;
+        for (Map.Entry<String, Integer> entry : basket.entrySet()) {
+            if (specialOffers.containsKey(new Pair<>(entry.getKey(), entry.getValue())))
+                total += specialOffers.get(new Pair<>(entry.getKey(), entry.getValue()));
+            else
+                total += prices.get(entry.getKey()) * entry.getValue();
         }
+
+        return total;
 
     }
-
 }
+
 
 
 
