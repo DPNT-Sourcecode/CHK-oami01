73af10f0456c9f2abacc8168e73cdfda2864c148
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 15:33:57 +0000

    Tue Feb 21 15:33:57 UTC 2023

--- src/main/java/befaster/solutions/CHK/MultiPricingOffers/MultiPricingOfferE.java
+++ src/main/java/befaster/solutions/CHK/MultiPricingOffers/MultiPricingOfferE.java
@@ -10,7 +10,7 @@ public class MultiPricingOfferE implements MultiPricingOffer {
     public Map<String,Integer> modifyBasket(Map<String, Integer> basket) {
         String product="E";
         Integer quantity=basket.get(product);
-        if(basket.get(product)>=2)
+        if(basket.get(product)>=2&&basket.containsKey("B"))
         {
             Integer newValue=basket.get("B")-quantity/2;
             basket.remove("B");
@@ -19,6 +19,7 @@ public class MultiPricingOfferE implements MultiPricingOffer {
         return basket;
     }
 }
+
 
 
 
