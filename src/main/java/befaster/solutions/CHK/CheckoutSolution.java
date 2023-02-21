7c3c59da03f99e5e456114d0b8b5866c6f871485
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 13:59:10 +0000

    Tue Feb 21 13:59:10 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,15 +4,33 @@ import befaster.runner.SolutionNotImplementedException;
 
 import java.util.AbstractMap;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import static jdk.nashorn.internal.runtime.JSType.toInteger;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        Map<String,Integer> prices=Map.ofEntries(
-                new AbstractMap.SimpleEntry<String,Integer>("A",)
+        Map<String, Integer> prices= (Map<String, Integer>) Stream.of(
+                new AbstractMap.SimpleEntry<String,Integer>("A",50) ,
+                new AbstractMap.SimpleEntry<String,Integer>("B",30),
+                new AbstractMap.SimpleEntry<String,Integer>("C",20),
+                new AbstractMap.SimpleEntry<String,Integer>("D",15)
         );
+
+        Map<String, Integer> specialOffers= (Map<String, Integer>) Stream.of(
+                new AbstractMap.SimpleEntry<String,Integer>("3A",130) ,
+                new AbstractMap.SimpleEntry<String,Integer>("2B",45)
+        );
+
+        if(specialOffers.containsKey(skus))
+            return specialOffers.get(skus);
+        else {
+            return (skus.charAt(0))*prices.get(skus.charAt(1));
+        }
 
     }
 }
+
 
 
