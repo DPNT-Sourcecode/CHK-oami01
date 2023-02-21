36e55ed009b98cbe73864323982b039482bace7d
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 14:13:19 +0000

    Tue Feb 21 14:13:19 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,34 +3,43 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static jdk.nashorn.internal.runtime.JSType.toInteger;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
+    public static Integer checkout(String skus) {
 
-        Map<String, Integer> prices= (Map<String, Integer>) Stream.of(
-                new AbstractMap.SimpleEntry<String,Integer>("A",50) ,
-                new AbstractMap.SimpleEntry<String,Integer>("B",30),
-                new AbstractMap.SimpleEntry<String,Integer>("C",20),
-                new AbstractMap.SimpleEntry<String,Integer>("D",15)
-        );
+        Map<String, Integer> prices = new HashMap<>();
+        prices.put("A", 50);
+        prices.put("B", 30);
+        prices.put("C", 20);
+        prices.put("D", 15);
 
-        Map<String, Integer> specialOffers= (Map<String, Integer>) Stream.of(
-                new AbstractMap.SimpleEntry<String,Integer>("3A",130) ,
-                new AbstractMap.SimpleEntry<String,Integer>("2B",45)
-        );
+        Map<String, Integer> specialOffers = new HashMap<>();
+        specialOffers.put("3A", 130);
+        specialOffers.put("2B", 45);
 
-        if(specialOffers.containsKey(skus))
+        if (specialOffers.containsKey(skus))
             return specialOffers.get(skus);
         else {
-            return (skus.charAt(0)-60)*prices.get(skus.charAt(1));
+            int pricePerProduct=prices.get(String.valueOf(skus.charAt(skus.length()-1)));
+            if(skus.length()>1)
+            {
+                skus=skus.substring(0,skus.length()-1);
+                return Integer.parseInt(skus)*pricePerProduct;
+            }
+            else{
+                return pricePerProduct;
+            }
+
         }
 
     }
 }
+
 
 
 
