5d7650d237c3020f231c4a87ec025389edf8deb3
sbx_user1051 <sbx_user1051@169.254.177.13>
2023-02-21 15:06:47 +0000

    Tue Feb 21 15:06:47 UTC 2023

--- src/main/java/befaster/solutions/CHK/SpecialOffers/SpecialOfferE.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffers/SpecialOfferE.java
@@ -8,7 +8,9 @@ public class SpecialOfferE implements SpecialOffer {
     public Integer getReward(Map<String, Integer> basket,Map<String, Integer> prices) {
         String product="E";
         Integer quantity=basket.get(product);
+        if(basket.containsKey("B"))
         basket.put("B",basket.get("B")-quantity/2);
         return quantity*prices.get(product);
     }
 }
+
